1be354415a1be7dc54285f4ffd1686390b19577e
AndreyT <zanarions@gmail.com>
2023-08-19 00:14:15 +0300

    bug

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -8,13 +8,55 @@ public class CreditAccountTest {
     @Test
     public void shouldAddToPositiveBalance() {
         CreditAccount account = new CreditAccount(
-                0,
+                2_000,
                 5_000,
                 15
         );
 
         account.add(3_000);
 
-        Assertions.assertEquals(3_000, account.getBalance());
+        Assertions.assertEquals(5_000, account.getBalance());
+    }
+
+    @Test
+    public void testYearChangeNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                -4_000,
+                5_000,
+                10);
+
+
+        int expected = 0;
+        int actual = account.yearChange();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPayOverCreditLimitBalance() {
+        CreditAccount account = new CreditAccount(
+                2000,
+                5_000,
+                10
+        );
+
+        account.pay(10_000);
+
+        int expected = 2000;
+
+        Assertions.assertEquals(expected, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddToNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                -2_000,
+                5_000,
+                15
+        );
+
+        account.add(3_000);
+
+        Assertions.assertEquals(1_000, account.getBalance());
     }
 }
