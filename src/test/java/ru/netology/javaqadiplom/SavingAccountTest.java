53a4b409571aecfeff87e9f2366ea2f04c60391b
Sergey30ssw <133129423+Sergey30ssw@users.noreply.github.com>
2023-08-21 12:01:26 +0400

    Update SavingAccountTest.java

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -3,19 +3,45 @@ package ru.netology.javaqadiplom;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class SavingAccountTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class CreditAccountTest {
 
     @Test
-    public void shouldAddLessThanMaxBalance() {
-        SavingAccount account = new SavingAccount(
-                2_000,
-                1_000,
-                10_000,
-                5
+    public void shouldAddToPositiveBalance() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
         );
 
         account.add(3_000);
 
-        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+        assertEquals(3_000, account.getBalance());
+    }
+    @Test
+    public void shouldAddToPositiveBalanceNegativeAmount() {
+        CreditAccount account = new CreditAccount(
+                1_000,
+                5_000,
+                15
+        );
+
+        account.add(-3_000);
+
+        Assertions.assertEquals(1000, account.getBalance());
+    }
+    @Test
+    public void testNegativePaymentAmount() { // проверка на негативное (меньше нуля) значение amount
+        int initialBalance = 10_000;
+        int creditLimit = 15_000;
+        int amount = -1000;
+
+        ru.netology.javaqadiplom.CreditAccount account = new ru.netology.javaqadiplom.CreditAccount(initialBalance, creditLimit, 10);
+        boolean result = account.pay(amount);
+        assertFalse(result);
+        assertEquals(10000, account.getBalance());
     }
 }
+
