ef8fd56b50c6ee72c905af1ac392976b460a7805
Sergey30ssw <133129423+Sergey30ssw@users.noreply.github.com>
2023-08-21 12:30:46 +0400

    Update CreditAccountTest.java

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -59,4 +59,18 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(1_000, account.getBalance());
     }
+    @Test
+    public void testPayPositiveBalance() {
+        CreditAccount account = new CreditAccount(
+                2_000,
+                5_000,
+                10
+        );
+
+        account.add(10_000);
+
+        int expected = 2_000;
+
+        Assertions.assertEquals(10_000, account.getBalance());
+    }
 }
