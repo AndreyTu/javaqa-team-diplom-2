5250725d61b21d2267e1c98e32ca28dcf4e9546b
Sergey30ssw <133129423+Sergey30ssw@users.noreply.github.com>
2023-08-23 18:37:16 +0400

    Update CreditAccount.java

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -23,12 +23,12 @@ public class CreditAccount extends Account {
                     "Накопительная ставка не может быть отрицательной, а у вас: " + rate
             );
         }
-        if (initialBalance <= 0) {
+        if (initialBalance <= 0) {// issue #9
             throw new IllegalArgumentException(
                     "Начальный баланс не может быть отрицательным, а у вас: " + initialBalance
             );
         }
-        if (creditLimit < 0) {
+        if (creditLimit < 0) { // issue #10
             throw new IllegalArgumentException(
                     "Кредитный лимит не может быть отрицательным, а у вас: " + creditLimit
             );
